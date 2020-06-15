179ed9a6d6ff6387cf2eff03162350d8b74c1d18
EktwrW <63172881+EktwrW@users.noreply.github.com>
2020-06-15 18:13:32 -0300

    Dibujando con Canvas
    Ejercicio Curso de Javascript.

---
+++ canvasss.js
@@ -0,0 +1,47 @@
+var texto = document.getElementById('texto_lineas');
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick );
+var d = document.getElementById("dibujito");
+var ancho = d.width;
+var lienzo = d.getContext("2d");
+
+
+function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final){
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(x_inicial, y_inicial);
+  lienzo.lineTo(x_final, y_final);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+function dibujoPorClick()
+{
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var yi, xf;
+  var xi, yf;
+  var colorB = "Blue";
+  var espacio = (ancho/2) / lineas;
+
+  for(l = 0; l < lineas; l++)
+    {
+        xi = espacio * l;
+        yf = 300 - (espacio * l);
+        dibujarLinea(colorB, xi, 300, 300, yf);
+
+        yi = (espacio * l) + 300;
+        xf = espacio * l;
+        dibujarLinea(colorB, 300, yi, xf, 300);
+
+        xi = (espacio * l) + 300;
+        yf = espacio * l;
+        dibujarLinea(colorB, xi, 300, 300, yf);
+
+        yi = 600 - (espacio * l);
+        xf = (espacio * l) + 300;
+        dibujarLinea(colorB, 300, yi, xf, 300);
+    }
+  dibujarLinea(colorB, 1,300,600,300);
+  dibujarLinea(colorB, 300,1,300,600);
+}
